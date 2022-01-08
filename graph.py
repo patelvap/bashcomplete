0b7d8a2a8d09b5ee61391929264ecf8f68036278
Vraj Patel <vapatel@vraj-pc.lan>
2022-01-07 21:12:44 -0500

    prediction algo code

---
+++ graph.py
@@ -0,0 +1,60 @@
+from thefuzz import fuzz
+from thefuzz import process
+
+import pprint
+pp = pprint.PrettyPrinter(indent = 4)
+
+class Node:
+
+    def __init__(self, program, frequency = 1) -> None:
+        ''' 
+        Args:
+            program (String): Program that Node represents
+            frequency (int): amount of times in data command proceeds parent_command
+           
+            command (dict): Full commands with frequencies to be used by parent nodes
+            children (dict): dict of children nodes, key command, value Node
+        '''
+        
+        self.program = program
+        self.frequency = frequency
+        self.commands = {} # key cmd, value frequency
+        self.children = {}
+
+    # take children with the X highest frequencies
+    # fuzzy match with all of the command dicts in child nodes
+    # weighing with frequency and ratio (multiplied)
+    # cut off the program
+    def get_prediction(self, previous_command) -> str:
+        
+        child_nodes = list(self.children.values())
+        child_nodes.sort(key = lambda node: node.frequency)
+        # get all child_nodes commands into one dict to sort
+        command_dicts = {}
+        for i in range(len(child_nodes)): 
+            command_dicts.update(child_nodes[i].commands)
+
+        potential_commands = sorted(command_dicts.items(), key=lambda item : item[1], reverse=True)
+        
+        #fuzzy match now with command parameter
+        print(potential_commands)
+        return_command = 'None'
+        highest_score = 0
+
+        for potential_command in potential_commands:
+
+            fuzzy_score = .5 * fuzz.ratio(previous_command, potential_command[0])
+
+            #set ratio to 1 incase no match; intent of fuzzy ratio is to match with args
+            if fuzzy_score == 0:
+                fuzzy_score = 1
+
+            cur_score = fuzzy_score * potential_command[1]
+
+            if (cur_score > highest_score):
+                return_command = potential_command[0]
+                highest_score = potential_command[1]
+                print(return_command)
+
+
+        return return_command
--- parse.py
+++ parse.py
@@ -1,8 +1,10 @@
 import bashlex
+import copy
 import os
 import re
 
-re_filter = "C "
+command_filter = "C "
+start_filter = "S"
 
 scientists_dir = "./unix-data/computer-scientists/"
 experienced_dir = "./unix-data/experienced-programmers/"
@@ -42,13 +44,13 @@ for file_path in scientists_files:
     lines = file1.readlines()
     
     for line in lines:
-        if re.match(re_filter, line) is not None:
+        if re.match(command_filter, line) is not None:
             scientist_commands.append(line[2:-1])
             try:
                 parts = bashlex.parse(scientist_commands[-1])
                 scientist_parsed.append(parts[0].dump())
             except Exception as inst:
-                print(scientist_commands[-1])
+                pass
 
 
 for file_path in experienced_files:
@@ -56,32 +58,55 @@ for file_path in experienced_files:
     lines = file1.readlines()
     
     for line in lines:
-        if re.match(re_filter, line) is not None:
+        if re.match(command_filter, line) is not None:
             experienced_commands.append(line[2:-1])
+        try:
             parts = bashlex.parse(experienced_commands[-1])
             experienced_parsed.append(parts[0].dump())
+        except Exception as inst:
+            pass
 
 for file_path in non_programmers_files:
     file1 = open(file_path, encoding="ISO-8859-1")
     lines = file1.readlines()
     
     for line in lines:
-        if re.match(re_filter, line) is not None:
+        if re.match(command_filter, line) is not None:
             non_programmers_commands.append(line[2:-1])
+        try:
             parts = bashlex.parse(non_programmers_commands[-1])
             non_programmers_parsed.append(parts[0].dump())
+        except Exception as inst:
+            pass
             
 for file_path in novice_files:
     file1 = open(file_path, encoding="ISO-8859-1")
     lines = file1.readlines()
     
     for line in lines:
-        if re.match(re_filter, line) is not None:
+        if re.match(command_filter, line) is not None:
             novice_commands.append(line[2:-1])
+        try:
             parts = bashlex.parse(novice_commands[-1])
             novice_parsed.append(parts[0].dump())
+        except Exception as inst:
+            pass
 
+# TODO: add logic so each session (between starts) is in its own list
+# DRAFT
+def parse_commands(command_list, parsed_list, files_list):
+    for file_path in files_list: 
+        file1 = open(file_path, encoding="ISO-8859-1")
+        lines = file1.readlines()
 
-print(novice_parsed)
+        for line in lines:
+            if re.match(command_filter, lines) is not None:
+                command_list.append(line[2:-1])
+            try:
+                parts = bashlex.parse(command_list[-1])
+                parsed_list.append(parts[0].dump())
+            except Exception as inst:
+                pass
 
-
+    return parsed_list
+            
